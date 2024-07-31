0675f5b0a867173cfe0f7b0602f27031adaf9588
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-31 20:58:49 +0200

    Refactor TicketDetailView to handle ticket retrieval and update, and fix response status bug

--- kanbanboard/views.py
+++ kanbanboard/views.py
@@ -101,3 +101,4 @@ class TicketDetailView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      
